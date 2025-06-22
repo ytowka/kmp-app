24bbc87e3ecdc096a66474ecfe5bfc6edcfb72e0
Danil Khairulin <enddonil@gmail.com>
2025-06-22 14:32:05 +0300

    core module setup refactor

--- sources/core/build.gradle.kts
+++ sources/core/build.gradle.kts
@@ -1,37 +1,10 @@
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.ksp)
     id("shared-koin")
+    id("module-setup")
 }
 
 kotlin {
-    androidLibrary {
-        namespace = "com.example.core"
-        compileSdk = 35
-        minSdk = 26
-    }
-
-    val xcfName = "coreKit"
-
-    iosX64 {
-        binaries.framework {
-            baseName = xcfName
-        }
-    }
-
-    iosArm64 {
-        binaries.framework {
-            baseName = xcfName
-        }
-    }
-
-    iosSimulatorArm64 {
-        binaries.framework {
-            baseName = xcfName
-        }
-    }
-
     sourceSets {
         commonMain {
             dependencies {
@@ -40,24 +13,16 @@ kotlin {
                 api(libs.kotlinx.coroutines.core)
                 api(libs.androidx.lifecycle.viewmodel)
                 api(libs.napier)
-                // Add KMP dependencies here
             }
         }
         androidMain {
             dependencies {
                 api(libs.kotlinx.coroutines.android)
-                // Add Android-specific dependencies here. Note that this source set depends on
-                // commonMain by default and will correctly pull the Android artifacts of any KMP
-                // dependencies declared in commonMain.
             }
         }
         iosMain {
             dependencies {
-                // Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
-                // Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
-                // part of KMP’s default source set hierarchy. Note that this source set depends
-                // on common by default and will correctly pull the iOS artifacts of any
-                // KMP dependencies declared in commonMain.
+
             }
         }
         commonTest {
